25f3e44baed0982cce4300ee614ac30cebda6127
Gabriela R <98127380+Aktana@users.noreply.github.com>
2022-06-22 13:03:37 +0300

    Update curs_06

--- Automation10_Projects/PythonAutomation/introduction_to_programming/exercitii_1.py
+++
@@ -1,6 +0,0 @@
-model = 123
-print(type(model))
-print(isinstance(model,(int,float)))
-while isinstance(model,(int,float))==True:  # verific daca inputul de la utilizator este un string
-    model = input('Invalid value, please try again.')
-

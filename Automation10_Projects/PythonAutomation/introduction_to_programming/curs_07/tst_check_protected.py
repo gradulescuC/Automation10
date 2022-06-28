2eed6533ab834351bf1c4ae1794ab9969e77f6b8
Gabriela R <98127380+Aktana@users.noreply.github.com>
2022-06-28 21:22:03 +0300

    Curs_07

---
+++ Automation10_Projects/PythonAutomation/introduction_to_programming/curs_07/tst_check_protected.py
@@ -0,0 +1,12 @@
+from Automation10_Projects.PythonAutomation.introduction_to_programming.curs_07.POO_piloni.tst_encapsulation import Student
+
+class Student_upgraded(Student):
+    _var = "test"
+    __va1 = "test_private"
+    def update_name(self):
+         self._name = "Aidan" # Aici variabila nume a fost disponibila in recomandari pentru ca suntem in in interiorul clasei mostenitoare
+
+std = Student
+# print(std._name) Aici ne da eroare: AttributeError:
+                    # type object 'Student' has no attribute '_name'
+                        # pentru ca suntem in afara pachetylui care contine clasa Student
